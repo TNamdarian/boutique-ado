8313d192fd5867caf201b7141eea9ddb24605dc5
TNamdarian <teemanamdarian@gmail.com>
2022-05-13 09:54:49 +0000

    Edit apps.py

--- products/models.py
+++ products/models.py
@@ -30,6 +30,5 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -5,9 +5,8 @@ from .models import Product
 
 
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries
-    """
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
 
     context = {
